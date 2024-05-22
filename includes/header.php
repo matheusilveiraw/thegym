bbb4058f1aa87bbd59202b5b30645641a382494c
matheus <matheus.silveiraw@gmail.com>
2024-05-22 16:09:21 -0300

    lista musculos sem inserts pronto + connect feito

--- includes/header.php
+++ includes/header.php
@@ -16,25 +16,4 @@
 <body class="d-flex flex-column min-vh-100">
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
-      <a class="navbar-brand" href="index.php">The Gym</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link" href="index.php">Início</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="clientes.php">Clientes</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="produtos.php">Produtos</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="compras.php">Compras</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
+
--- index.php
+++ index.php
@@ -2,11 +2,34 @@
     include_once 'includes/header.php';
 ?>
 
+<a class="navbar-brand" href="index.php">The Gym</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link" href="index.php">Início</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="./musculos/musculos_lista.php">Músculos</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="produtos.php"> --- </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="compras.php"> --- </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
 <div class="grid text-center mt-5 flex-grow-1">
     <div class="col-12">
         <div class="container mt-4">
-            <h1>Bem-vindo ao Projeto CRUD</h1>
-            <p>Este é um projeto de exemplo para demonstrar operações CRUD (Create, Read, Update, Delete).</p>
+            <h2>Bem-vindo ao Projeto CRUD</h2>
+            <p>Este é um projeto de exemplo para demonstrar meus conhecimentos em PHP, SQL, HTML, CSS, Bootstrap e JavaScript, onde estarei simulando um sistema de academia.</p>
             <p>Utilize a barra de navegação acima para explorar o site.</p>
         </div>
     </div>
