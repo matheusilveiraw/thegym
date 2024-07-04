167b19fd25b03515f376f0513746dc571658d35d
matheus <matheus.silveiraw@gmail.com>
2024-07-04 13:49:06 -0300

    erro na lista

--- treino/lista_treino.php
+++ treino/lista_treino.php
@@ -66,10 +66,10 @@ include_once '../banco_de_dados/connect.php';
                 FROM treino AS t
                 INNER JOIN exercicio AS e1 ON t.exercicio1 = e1.id
                 INNER JOIN exercicio AS e2 ON t.exercicio2 = e2.id
-                INNER JOIN exercicio AS e3 ON t.exercicio2 = e3.id
-                INNER JOIN exercicio AS e4 ON t.exercicio1 = e4.id
-                INNER JOIN exercicio AS e5 ON t.exercicio2 = e5.id
-                INNER JOIN exercicio AS e6 ON t.exercicio2 = e6.id;";
+                INNER JOIN exercicio AS e3 ON t.exercicio3 = e3.id
+                INNER JOIN exercicio AS e4 ON t.exercicio4 = e4.id
+                INNER JOIN exercicio AS e5 ON t.exercicio5 = e5.id
+                INNER JOIN exercicio AS e6 ON t.exercicio6 = e6.id;";
 
         $resultado = mysqli_query($connect, $sql);
 
