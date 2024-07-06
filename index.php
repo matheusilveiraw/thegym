c42a519e5e7264d03de3fa8156b76b95ef4f4b06
Matheus Silveira <matheus.silveiraw@gmail.com>
2024-07-06 15:12:56 -0300

    descrição do index atualizada

--- index.php
+++ index.php
@@ -35,8 +35,8 @@
 <div class="grid text-center mt-5 flex-grow-1">
     <div class="col-12">
         <div class="container mt-4">
-            <h2>Bem-vindo ao Projeto CRUD</h2>
-            <p>Este é um projeto de exemplo para demonstrar meus conhecimentos em PHP, SQL, HTML, CSS, Bootstrap e JavaScript, onde estarei simulando um sistema de academia.</p>
+            <h2>Bem-vindo ao THE GYM</h2>
+            <p>Este é um projeto para representar o sistema de uma academia da perspectiva do administrador para demonstrar meus conhecimentos em PHP, PHP orientado à objetosm, SQL, HTML, CSS, Bootstrap e JavaScript. </p>
             <p>Utilize a barra de navegação acima para explorar o site.</p>
         </div>
     </div>
