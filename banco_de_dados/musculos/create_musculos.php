cadd14ddba55e2c4689369243d8ffcfaa53fba83
Matheus Silveira <matheus.silveiraw@gmail.com>
2024-07-14 17:22:19 -0300

    erro ao passar codigo html cadastro musculo

--- banco_de_dados/musculos/create_musculos.php
+++ banco_de_dados/musculos/create_musculos.php
@@ -2,17 +2,22 @@
     require_once '../../vendor/autoload.php';
     require_once '../connect.php';
 
-    if(isset($_POST['btn-cadastrar-musculos'])) { //verifica se o btn foi apertado
-        //pegar os dados do formulario passado por submit
-        $musculo = new \App\Model\Musculo();
+    $nome = $_POST['nome'];
+    $link = $_POST['link'];
 
-        $musculo -> setNome($_POST['nome']);
-        $musculo -> setLink($_POST['link']);
-        $musculo -> setData(date('Y-m-d H:i:s'));
-
-        $musculoDao = new \App\Model\MusculoDao();
-
-        $musculoDao -> create($musculo);
+    if(htmlspecialchars($nome, ENT_QUOTES, 'UTF-8') || htmlspecialchars($link, ENT_QUOTES, 'UTF-8')) {
+        header('Location: ../../musculos/musculos_lista.php?passouCodigoHtml');
+    } else { 
+        if(isset($_POST['btn-cadastrar-musculos'])) { 
+            $musculo = new \App\Model\Musculo();
+    
+            $musculo -> setNome($nome);
+            $musculo -> setLink($link);
+            $musculo -> setData(date('Y-m-d H:i:s'));
+    
+            $musculoDao = new \App\Model\MusculoDao();
+    
+            $musculoDao -> create($musculo);
+        }
     }
-
 ?>
