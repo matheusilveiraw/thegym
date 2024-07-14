6fbe4b579851fbda54d3664e06ef362945b725e6
Matheus Silveira <matheus.silveiraw@gmail.com>
2024-07-14 17:43:02 -0300

    eliminado html do atualizar exercicios

--- banco_de_dados/musculos/update_musculos.php
+++ banco_de_dados/musculos/update_musculos.php
@@ -1,17 +1,19 @@
 <?php
-    require_once '../../vendor/autoload.php';
-    require_once '../connect.php';
+require_once '../../vendor/autoload.php';
+require_once '../connect.php';
+require_once '../../includes/limparHtml.php';
 
-    if(isset($_POST['btn-cadastrar-musculos'])) {
-        $musculo = new \App\Model\Musculo();
+$nome = limparHtml($_POST['nome']);
+$link = limparHtml($_POST['link']);
 
-        $musculo -> setId($_POST['id']);
-        $musculo -> setNome($_POST['nome']);
-        $musculo -> setLink($_POST['link']);
+if (isset($_POST['btn-cadastrar-musculos'])) {
+    $musculo = new \App\Model\Musculo();
 
-        $musculoDao = new \App\Model\MusculoDao();
+    $musculo->setId($_POST['id']);
+    $musculo->setNome($nome );
+    $musculo->setLink($link);
 
-        $musculoDao -> update($musculo);
-    }
+    $musculoDao = new \App\Model\MusculoDao();
 
-?>
+    $musculoDao->update($musculo);
+}
